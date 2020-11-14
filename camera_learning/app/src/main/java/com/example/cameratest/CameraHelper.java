fcf25fa8316a1240f67e284eae275da031ced63e
zjz1988314 <zjz1988314@163.com>
2020-11-14 17:26:25 +0800

    add camera_learning.

---
+++ camera_learning/app/src/main/java/com/example/cameratest/CameraHelper.java
@@ -0,0 +1,190 @@
+package com.example.cameratest;
+
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+
+import android.content.Context;
+import android.app.Activity;
+
+import android.media.Image;
+import android.media.ImageReader;
+import android.graphics.ImageFormat;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import android.util.Log;
+
+
+public class CameraHelper /*implements MediaRecorder.OnErrorListener, MediaRecorder.OnInfoListener*/ {
+    private final static String TAG = "CameraHelper";
+
+    private SurfaceHolder mSurfaceHolder;
+    //private SurfaceTexture mSurfaceTexture;
+    private SurfaceView mSurfaceView;
+
+    //private ImageReader mImageReader;
+    private CameraCaptureSession mPreviewSession;
+
+    private CameraDevice mCameraDevice;
+    private CaptureRequest.Builder mPreviewBuilder;
+    private Handler mCameraHandler;
+    private int mPreviewWidth = 1280;
+    private int mPreviewHeight = 720;
+
+    private Context mContext;
+
+    public CameraHelper(Context ctx, SurfaceView sv) {
+        Log.d(TAG, "CameraHelper ctor begin!");
+        mContext = ctx;
+
+        mSurfaceView = sv;
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.setFixedSize(mPreviewWidth, mPreviewHeight);
+        mSurfaceHolder.addCallback(mSurfaceHolderCallback);
+
+        HandlerThread handlerThread = new HandlerThread("CameraHelper");
+        handlerThread.start();
+        mCameraHandler = new Handler(handlerThread.getLooper());
+        Log.d(TAG, "CameraHelper ctor end!");
+    }
+
+    private SurfaceHolder.Callback mSurfaceHolderCallback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            Log.d(TAG, "surfaceCreated! this=" + this + ", holder=" + holder);
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            Log.d(TAG, "surfaceChanged! this=" + this + ", holder=" + holder);
+            Log.d(TAG, "format=["+format+"], width/height=["+width+"/"+height+"]");
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            Log.d(TAG, "surfaceDestroyed! this=" + this + ", holder=" + holder);
+        }
+    };
+
+    public void openCamera() {
+        Log.d(TAG, "openCamera begin!");
+        CameraManager camMgr = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            CameraCharacteristics cc = camMgr.getCameraCharacteristics(Integer.toString(CameraCharacteristics.LENS_FACING_FRONT));
+            StreamConfigurationMap map = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            assert map!=null;
+
+            //mImageReader = ImageReader.newInstance(mPreviewWidth, mPreviewHeight, ImageFormat.JPEG, 1);
+            //mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, mCameraHandler);
+
+            camMgr.openCamera(Integer.toString(CameraCharacteristics.LENS_FACING_FRONT), mCamDevStateCB, mCameraHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "openCamera end!");
+    }
+
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image img = reader.acquireNextImage();
+            Log.d(TAG, "a image come! h/w:[" + img.getWidth() + "/" + img.getHeight() + "]");
+            img.close();
+        }
+    };
+
+    private CameraDevice.StateCallback mCamDevStateCB = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice cd) {
+            try {
+                Log.i(TAG, "onOpened begin!");
+                mCameraDevice = cd;
+                Surface previewSurface = mSurfaceHolder.getSurface();
+                //Surface imageSurface = mImageReader.getSurface();
+                mPreviewBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                mPreviewBuilder.addTarget(previewSurface);
+                //mPreviewBuilder.addTarget(imageSurface);
+                //mCameraDevice.createCaptureSession(Arrays.asList(previewSurface, imageSurface), mCapStateCB, mCameraHandler);
+                mCameraDevice.createCaptureSession(Arrays.asList(previewSurface), mCapStateCB, mCameraHandler);
+                Log.d(TAG, "camDev=" + cd + ", previewSurface=" + previewSurface);
+                Log.i(TAG, "onOpened end!");
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private CameraCaptureSession.StateCallback mCapStateCB = new CameraCaptureSession.StateCallback() {
+            @Override
+            public void onConfigured(CameraCaptureSession session) {
+                Log.i(TAG, "onConfigured begin!");
+                mPreviewSession = session;
+                try {
+                    mPreviewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+                    CaptureRequest request = mPreviewBuilder.build();
+                    mPreviewSession.setRepeatingRequest(request, null, mCameraHandler);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+                Log.i(TAG, "onConfigured end!");
+            }
+
+            @Override
+            public void onConfigureFailed(CameraCaptureSession session) {
+                Log.d(TAG, "onConfiguredFailed");
+            }
+        };
+
+        @Override
+        public void onDisconnected(CameraDevice camDev) {
+            Log.d(TAG, "onDisconnected");
+            camDev.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(CameraDevice camDev, int err) {
+            Log.e(TAG, "onError");
+            camDev.close();
+            mCameraDevice = null;
+        }
+    };
+
+    public void closePreviewSession() {
+        if (mPreviewSession != null) {
+            mPreviewSession.close();
+            mPreviewSession = null;
+        }
+    }
+
+    public void closeCameraDevice() {
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    }
+
+    public void stopCameraAndPreview() {
+        Log.w(TAG, "stopPreview begin!");
+        closeCameraDevice();
+        closePreviewSession();
+        Log.w(TAG, "stopPreview end!");
+    }
+
+    public void destroy() {
+        Log.w(TAG, "destroy begin!");
+        stopCameraAndPreview();
+        Log.w(TAG, "destroy end!");
+    }
+}
---
+++ camera_learning/app/src/main/java/com/example/cameratest/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.cameratest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.app.AlertDialog;
+//import android.support.v4.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+import android.view.View;
+import android.view.TextureView;
+import android.view.SurfaceView;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import android.os.EnvironmentEx;
+import android.os.Environment;
+
+
+public class MainActivity extends Activity {
+    private static final String TAG = "CameraTest";
+
+    Context mContext;
+
+    CameraHelper mCamHelper;
+    int mCurCamState = 0; // 1-on; 0-off
+    SurfaceView mSurfaceView;
+
+    private Button button1;     //start
+    private Button button2;     //stop
+    private Button button3;     //current camera state
+    private Button button4;     //quit
+
+    @Override
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.main_activity);
+        Log.d(TAG, "onCreate");
+
+        mContext = this;
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surview);
+
+        // start preview
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setText("start-preview");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurCamState == 0) {
+                    if (mCamHelper == null) {
+                        Log.d(TAG, "CameraHelper ctor! start preview!");
+                        mCamHelper = new CameraHelper(mContext, mSurfaceView);
+                    }
+                    mCamHelper.openCamera();
+                    mCurCamState = 1;
+                    button3.setText("state:" + mCurCamState);
+                }
+            }
+        });
+
+        // stop preview
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setText("stop-preview");
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurCamState == 1) {
+                    Log.d(TAG, "stop camera device!");
+                    mCamHelper.closeCameraDevice();
+                    mCurCamState = 0;
+                    button3.setText("state:" + mCurCamState);
+                }
+            }
+        });
+
+        // camera state
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String state = EnvironmentEx.getExternalStoragePathState();
+                boolean mount = Environment.MEDIA_MOUNTED.equals(state);
+                Log.d(TAG, "state=" + state + ", mount=" + mount);
+            }
+        });
+
+        // quit Activity
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setText("quit Activity");
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w(TAG, "quit Activity!");
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        Log.w(TAG, "finish Activity!");
+    }
+}
